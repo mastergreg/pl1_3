ed6b6f8e6ae116cf6db9a002a74361e820a1f9f8
mastergreg <gregliras@gmail.com>
2011-07-05 17:27:46 +0300

    used list of chars instead of list of strings

--- Prolog/part_3/mama_mia.pl
+++ Prolog/part_3/mama_mia.pl
@@ -6,15 +6,15 @@
 % 
 % * Creation Date : 28-06-2011
 % 
-% * Last Modified : Tue 05 Jul 2011 05:10:47 PM EEST
+% * Last Modified : Tue 05 Jul 2011 05:25:59 PM EEST
 % 
 % * Created By : Greg Liras <gregliras@gmail.com>
 % 
 % _._._._._._._._._._._._._._._._._._._._._.*/
 
-  execuTe(_,_,I,OM,""):- OM=I.
-  execuTe(_,M,I,OM,"M"):- OM is M*I.
-  execuTe(A,_,I,OM,"A"):- OM is A+I.
+  execuTe(_,_,I,OM,''):- OM=I.
+  execuTe(_,M,I,OM,'M'):- OM is M*I.
+  execuTe(A,_,I,OM,'A'):- OM is A+I.
 
   big_sista(_,_,OMO,OMO,[]).
   big_sista(A,M,I  ,O  ,[P|Prog]):-
@@ -36,12 +36,11 @@
         big_mama(A,M,LI,HI,LO,HO,LIMIT,RProg,RetProg)
     ).
 
-  moreProg(IProg,OProg):-OProg=["M"|IProg];OProg=["A"|IProg].
+  moreProg(IProg,OProg):-OProg=['M'|IProg];OProg=['A'|IProg].
 
   mama_mia(A,M,LI,HI,LO,HO,Prog):-
     LIMIT1 is HO/A,
     LIMIT2 is ceiling(log(HO)/log(M)),
     LIMIT is min(LIMIT1,LIMIT2),
     big_mama(A,M,LI,HI,LO,HO,LIMIT,[],SProg),
-    flatten(SProg,FProg),
-    name(Prog,FProg).
+    atom_chars(Prog,SProg).
