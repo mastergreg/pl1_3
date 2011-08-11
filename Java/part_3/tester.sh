07030c99672969d9912a44c70a60e33917f8690b
mastergreg <gregliras@gmail.com>
2011-08-11 21:18:23 +0300

    just edited makefile

--- Java/part_3/tester.sh
+++ Java/part_3/tester.sh
@@ -6,7 +6,7 @@ tput sgr0                               # Reset colors to "normal."
 if [[ $test1Solution == $test1 ]]; then
   echo -e $run1 '\E[0;32mPass'
 else
-  echo -e $run1 '\E[0;31mFail'
+  echo -e $run1 '\E[0;31mFail\n'$test1Solution" expected but got "$test1
 fi
 tput sgr0                               # Reset colors to "normal."  
 
